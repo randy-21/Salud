41f647012c37ed297ea1aa0a7bb8f90b3014bc15
Anthonycode <logicainformatica18@gmail.com>
2024-12-24 18:53:27 -0500

    add photo template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://www.minsur.com/wp-content/uploads/2021/07/salud-22-23.png"width="200px" class="logo" alt="Laravel Logo">
+<img src="https://www.minsur.com/wp-content/uploads/2021/07/salud-22-23.png"width="100%" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
