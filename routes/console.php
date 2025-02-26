28341c1ebcd3a287a8fda6cef2dd9b0bdb8a97a2
logicainformatica18 <logicainformatica18@gmail.com>
2025-02-25 21:04:13 -0500

    add job

--- routes/console.php
+++ routes/console.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 use App\Jobs\SendCiteReminder;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// 📅 Registrar el Job para ejecutarse dos veces al día (8 AM y 8 PM)
+// 📅 Registrar el Job para ejecutarse a las 8 AM y 8 PM
 app()->booted(function () {
     $schedule = app(Schedule::class);
-    $schedule->job(new SendCiteReminder())->twiceDaily(8, 20);
+    $schedule->job(new SendCiteReminder())->twiceDaily(8, 20); // Ejecuta a las 8 AM y 8 PM
 });
